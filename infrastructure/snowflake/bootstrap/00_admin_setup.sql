75ce0dc3ae4f770ecf33610747eaed2203eef6fd
RM3006 <romain.menant7@gmail.com>
2025-12-20 14:46:25 +0100

    chore(infra): fixed missing steps at the end of admin_setup file in infrastructure/snowflake/bootstrap

--- infrastructure/snowflake/bootstrap/00_admin_setup.sql
+++ infrastructure/snowflake/bootstrap/00_admin_setup.sql
@@ -1,11 +1,25 @@
--- 1. Create a specialized role for your CI/CD pipeline
+/* --------------------------------------------------------------------------------
+   BOOTSTRAP SCRIPT (MANUAL RUN ONLY)
+   Run as: ACCOUNTADMIN
+   Description: Sets up the CI/CD Role needed for GitHub Actions.
+   --------------------------------------------------------------------------------
+*/
+
 USE ROLE SECURITYADMIN;
+
+-- 1. Create the specialized role for the pipeline
 CREATE ROLE IF NOT EXISTS ci_cd_role;
 
--- 2. Give it the power to "Hire" (Security Admin privileges)
+-- 2. Grant privileges to manage roles
 GRANT CREATE ROLE ON ACCOUNT TO ROLE ci_cd_role;
-GRANT MANAGE GRANTS ON ACCOUNT TO ROLE ci_cd_role; 
--- (MANAGE GRANTS is powerful: allows modifying privileges it doesn't own)
+GRANT MANAGE GRANTS ON ACCOUNT TO ROLE ci_cd_role;
 
--- 3. Give it the power to "Build" (Sysadmin hierarchy)
+-- 3. Grant privileges to manage objects (Build)
 GRANT ROLE SYSADMIN TO ROLE ci_cd_role;
+
+-- 4. Grant usage on the warehouse 
+GRANT USAGE ON WAREHOUSE COMPUTE_WH TO ROLE ci_cd_role; 
+
+-- 5. Assign this role to relevant user
+SET my_user = CURRENT_USER();
+GRANT ROLE ci_cd_role TO USER identifier($my_user);
