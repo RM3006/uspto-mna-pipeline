d06386b34e95263a16a4e22cba086530a422e7c7
RM3006 <romain.menant7@gmail.com>
2025-12-22 16:41:03 +0100

    chore(ingestion): fixed column order for table patent_assignment_xml

--- infrastructure/snowflake/deploy/03_create_raw_tables.sql
+++ infrastructure/snowflake/deploy/03_create_raw_tables.sql
@@ -6,7 +6,7 @@ USE SCHEMA raw;
 -- VARIANT column handles semi-structured XML data
 CREATE OR ALTER TABLE patent_assignment_xml (
     file_name VARCHAR,
-    loaded_at TIMESTAMP_NTZ DEFAULT to_timestamp_ntz(convert_timezone('Europe/Paris',  current_timestamp())),
-    xml_content VARIANT
+    xml_content VARIANT,
+    loaded_at TIMESTAMP_NTZ DEFAULT to_timestamp_ntz(convert_timezone('Europe/Paris',  current_timestamp()))
 )
 ;
