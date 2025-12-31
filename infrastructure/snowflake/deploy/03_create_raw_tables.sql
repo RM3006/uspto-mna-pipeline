fd5bf0d2af898f29b5db3ae14e8a7b69dbb0b56a
RM3006 <romain.menant7@gmail.com>
2025-12-31 11:45:47 +0100

    chore(infra): modified ddl for rax data table for xml data

--- infrastructure/snowflake/deploy/03_create_raw_tables.sql
+++ infrastructure/snowflake/deploy/03_create_raw_tables.sql
@@ -2,15 +2,11 @@ USE ROLE uspto_role;
 USE DATABASE uspto_db;
 USE SCHEMA raw;
 
--- 1. Create table to store raw XML content
+-- Create table to store raw XML content
 -- VARIANT column handles semi-structured XML data
-CREATE TABLE IF NOT EXISTS patent_assignment_xml (
+CREATE OR REPLACE TABLE patent_assignment_xml (
     file_name VARCHAR,
     xml_content VARIANT,
-    loaded_at TIMESTAMP_NTZ
+    loaded_at TIMESTAMP_NTZ DEFAULT to_timestamp_ntz(convert_timezone('Europe/Paris', current_timestamp()))
 );
 
--- 2. Explicitly apply the default value to the column
-ALTER TABLE patent_assignment_xml 
-MODIFY COLUMN loaded_at 
-SET DEFAULT to_timestamp_ntz(convert_timezone('Europe/Paris', current_timestamp()));
